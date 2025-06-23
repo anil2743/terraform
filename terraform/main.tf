fe9cb3683f880a80295dda53c0f6d162dd938a07
anil2743 <nakkaanilyadav2743@gmail.com>
2025-06-23 17:20:30 +0530

    god

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh_http" {
-  name_prefix = "allow-ssh-http-"  # Let Terraform generate a unique name
+  name_prefix = "allow-ssh-http-"
 
   ingress {
     from_port   = 22
@@ -24,17 +24,17 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 LTS (Mumbai)
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = [aws_security_group.allow_ssh_http.name]
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  security_groups        = [aws_security_group.allow_ssh_http.name]
 
   user_data_replace_on_change = true
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
               apt install apache2 -y
-              echo "<h1>Hello HERE ANIL YADAV  last update from Terraform + GitHub CI/CD</h1>" > /var/www/html/index.html
+              echo "<h1>Hello FROM USER DATA (first run only)</h1>" > /var/www/html/index.html
               systemctl start apache2
               systemctl enable apache2
             EOF
@@ -45,5 +45,18 @@ resource "aws_instance" "web" {
 
   lifecycle {
     ignore_changes = [user_data]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '<h1>UPDATED PAGE — from Anil Yadav via remote-exec</h1>' | sudo tee /var/www/html/index.html"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/terraform1.pem")  # ✅ File inside the project folder
+      host        = self.public_ip
+    }
   }
 }
